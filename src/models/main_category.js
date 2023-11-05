f2b9e4a3893a1fb0e4dbc68306113aa320a5212f
Mostafa Kamel <mostafamt60@yahoo.com>
2023-11-05 12:20:53 +0200

    update category

--- src/models/main_category.js
+++ src/models/main_category.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const MainCategorySchema = new Schema({
+const MainCategorySchema = new Schema(
+  {
     name: {
-        type: String
+      type: String,
+    },
+    description: {
+      type: String,
     },
     view: {
-        type: Boolean
+      type: Boolean,
     },
-    products: [{ type: mongoose.Types.ObjectId, ref: 'product' }]
-}
-, 
-{
-    timestamps:true
-})
+    products: [{ type: mongoose.Types.ObjectId, ref: "product" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-module.exports = mongoose.model.MainCategory || mongoose.model("MainCategory", MainCategorySchema);
+module.exports =
+  mongoose.model.MainCategory ||
+  mongoose.model("MainCategory", MainCategorySchema);
