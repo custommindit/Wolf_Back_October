78fa81a736b68cfe96b6bb1e417d74a13518451d
Mostafa Kamel <mostafamt60@yahoo.com>
2023-11-08 15:55:29 +0200

    remove auth for getting products, categories and sub-categories

--- src/routes/api/subCategory.js
+++ src/routes/api/subCategory.js
@@ -4,11 +4,16 @@ const subCategoryController = require("../../controllers/sub_category");
 
 router.post("/create", subCategoryController.add_subcategory);
 router.get(
-  "/main_category/:id", checkToken([roles.user]),
+  "/main_category/:id",
+  checkToken([roles.user]),
   subCategoryController.get_subcategory_by_main_category2
 );
-router.get("/main_subcategory/", checkToken([roles.user]), subCategoryController.get_subCategory);
-router.get("/count/:id",checkToken([roles.user]), subCategoryController.gettotalcount);
+router.get("/main_subcategory/", subCategoryController.get_subCategory);
+router.get(
+  "/count/:id",
+  checkToken([roles.user]),
+  subCategoryController.gettotalcount
+);
 router.put("/:id", subCategoryController.update_subcategory);
 
 module.exports = router;
