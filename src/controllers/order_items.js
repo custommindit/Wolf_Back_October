ca4ac1fba65d86a32ae8d18470c4cd244624a5a1
Rahma <rahmaramadan01007@example.com>
2023-11-22 15:01:50 +0200

    wallet addd

--- src/controllers/order_items.js
+++ src/controllers/order_items.js
@@ -3,6 +3,7 @@ const Cart = require("../models/cart");
 const Product = require("../models/product.js");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User.js");
 // const emailController = require("./ordermail");
 require("dotenv").config();
 
@@ -161,6 +162,8 @@ module.exports.Update_order_item = async (req, res) => {
     .then((e) => {
       ///   if(e.returnrequest==="accepted"|| e.returnrequest==="denied"){
       ///    emailController.returnsMail(e.firstName,e._id,e.returnrequest)}
+      const returnedMonyTOUser =await User.findByIdAndUpdate({_id:oi.user_id},{wallet:wallet+oi.totalPrice})
+      console.log(returnedMonyTOUser);
       return res.status(200).json(e);
     })
     .catch((err) => {
